b639a3ec65bea3d9cf66965ebfa1fcde6d01cffb
Nick Nguyen <nick.nguyen@sjsu.edu>
2023-04-09 03:47:23 -0700

    Model and Factory - Added code for Simulation and SimulationFactory

--- src/simstation/SimulationFactory.java
+++ src/simstation/SimulationFactory.java
@@ -1,5 +1,49 @@
 package simstation;
 
-public class SimulationFactory {
-    
+import mvc.AppFactory;
+import mvc.Command;
+import mvc.Model;
+import mvc.View;
+
+public class SimulationFactory implements AppFactory {
+
+    @Override
+    public Model makeModel() {
+        return null;
+    }
+
+    @Override
+    public View makeView(Model model) {
+        return new SimulationView(model);
+    }
+
+    @Override
+    public String[] getEditCommands() {
+        return new String[]{"Start", "Suspend", "Resume", "Stop", "Stats"};
+    }
+
+    @Override
+    public Command makeEditCommand(Model model, String type, Object source) {
+        if (type.equals("Start")) return new StartCommand(model);
+        if (type.equals("Suspend")) return new SuspendCommand(model);
+        if (type.equals("Resume")) return new ResumeCommand(model);
+        if (type.equals("Stop")) return new StopCommand(model);
+        if (type.equals("Stats")) return new StatsCommand(model);
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+    @Override
+    public String[] getHelp() {
+        return new String[0];
+    }
+
+    @Override
+    public String about() {
+        return null;
+    }
 }
