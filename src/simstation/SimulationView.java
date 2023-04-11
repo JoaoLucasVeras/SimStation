29dc3b8184c649ff28e35510cb48720591c5f26e
JoaoLucasVeras <joao.lucas.veras9@gmail.com>
2023-04-10 22:18:46 -0700

    Sim Panel and View 2nd

--- src/simstation/SimulationView.java
+++ src/simstation/SimulationView.java
@@ -1,5 +1,8 @@
 package simstation;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import mvc.Model;
 import mvc.View;
 
@@ -7,5 +10,21 @@ public class SimulationView extends View {
 
     public SimulationView(Model model) {
         super(model);
+    }
+    
+    public void paintComponent(Graphics gc) 
+    {
+    	super.paintComponent(gc);
+    	Color oldColor = gc.getColor();
+    	
+    	Simulation sim = (Simulation)model;
+    	if(sim.agents != null) {
+	    	for(Agent a: sim.agents) 
+	    	{
+	    		a.draw((Graphics)gc);
+	    	}
+    	}
+    	
+    	gc.setColor(oldColor);
     }
 }
