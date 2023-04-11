50353d12ce5cccf695f05bdcc7e696c5547b6e50
JoaoLucasVeras <joao.lucas.veras9@gmail.com>
2023-04-10 22:08:12 -0700

    Revert "Sim Panel and View"
    This reverts commit d3b77d5db6598e3687d369108796c2fc499b4885.

--- src/simstation/SimulationView.java
+++ src/simstation/SimulationView.java
@@ -1,8 +1,5 @@
 package simstation;
 
-import java.awt.Color;
-import java.awt.Graphics;
-
 import mvc.Model;
 import mvc.View;
 
@@ -10,22 +7,5 @@ public class SimulationView extends View {
 
     public SimulationView(Model model) {
         super(model);
-        
-    }
-    
-    public void paintComponent(Graphics gc) 
-    {
-    	super.paintComponent(gc);
-    	Color oldColor = gc.getColor();
-    	
-    	Simulation sim = (Simulation)model;
-    	if(sim.agents != null) {
-	    	for(Agent a: sim.agents) 
-	    	{
-	    		a.draw((Graphics)gc);
-	    	}
-    	}
-    	
-    	gc.setColor(oldColor);
     }
 }
