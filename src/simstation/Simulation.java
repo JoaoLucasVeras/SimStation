25d55fd0c6add0c42bbf958d8dcd77b2e1f0d94a
meteor <kyawsoehan.789@gmail.com>
2023-04-10 18:41:32 -0700

    added real time clock to simulation

--- src/simstation/Simulation.java
+++ src/simstation/Simulation.java
@@ -5,20 +5,42 @@ import mvc.Model;
 import mvc.Utilities;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Simulation extends Model {
+    private Timer timer;
     protected List<Agent> agents;
     protected int clock;
 
+    private void startTimer(){
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new ClockUpdater(), 1000, 1000);
+    }
+
+    private class ClockUpdater extends TimerTask{
+        public void run(){
+            clock++;
+        }
+    }
+
+    private void stopTimer(){
+        timer.cancel();
+        timer.purge();
+    }
+
     public void start() { for (Agent a: agents) {
+        clock = 0;
+        startTimer();
         agents.clear();
         this.populate();
         Thread thread = new Thread(a);
         thread.start();
     } }
-    public void suspend() { for (Agent a: agents) a.suspend(); }
-    public void resume() { for (Agent a: agents) a.resume(); }
-    public void stop() { for (Agent a: agents) a.stop(); }
+    public void suspend() { stopTimer(); for (Agent a: agents) a.suspend(); }
+    public void resume() { startTimer(); for (Agent a: agents) a.resume(); }
+    public void stop() { stopTimer(); for (Agent a: agents) a.stop(); }
+
     public Agent getNeighbor(Agent agent, double radius) {
         int start = Utilities.rng.nextInt(agents.size());
         int index = start;
@@ -26,7 +48,9 @@ public class Simulation extends Model {
             Agent target = agents.get(index);
             if (Math.hypot(target.getY()-agent.getY(),
                     target.getX()-agent.getX()) < radius) {
+                if(!target.equals(agent)){   
                 return target;
+                }
             }
             if (index==agents.size()) { index = 0; }
         } while (++index != start);
